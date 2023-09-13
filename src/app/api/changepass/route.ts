bb8a330f6e8d1c6209122ac6c347822497e237bf
Otzzu <mesachharmasendro@gmail.com>
2023-09-13 21:00:01 +0700

    feat(auth): Add endpoint for change password

---
+++ src/app/api/changepass/route.ts
@@ -0,0 +1,34 @@
+import { prisma } from "@/lib/db"
+import { hash } from "bcrypt"
+import { NextResponse } from "next/server"
+
+export async function PATCH(req: Request) {
+  try {
+    const { email, newPassword } = await req.json()
+
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email: email
+      }
+    })
+
+    if (!existingUser) return NextResponse.json({ message: "There is no user with this email" }, { status: 404 })
+
+    const hashPassword = await hash(newPassword, 10)
+
+    const updatedUser = await prisma.user.update({
+      where: {
+        email: email
+      },
+      data: {
+        password: hashPassword
+      }
+    })
+
+    const { password, ...rest } = updatedUser
+
+    return NextResponse.json({ user: rest, message: "Success change password" }, { status: 200 })
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message }, { status: 500 })
+  }
+}
