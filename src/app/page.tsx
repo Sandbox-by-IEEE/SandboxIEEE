d34f6323cdc9684675265aa1eedd3a39585ab4ae
Alvin Christopher Santausa <alvinchrisantausa@gmail.com>
2023-09-15 19:33:15 +0700

    refactor: props added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,44 @@
+'use client';
+import React, { useState } from 'react';
+
 import RadioButtons from '@/components/radio';
 
 export default function Home() {
+  const [selectedOption, setSelectedOption] = useState<string | null>(null);
+
+  const handleOptionChange = (value: string) => {
+    setSelectedOption(value);
+  };
+
+  const options = [
+    {
+      value: 'option1',
+      label: 'Option 1',
+      checked: selectedOption === 'option1',
+      onChange: handleOptionChange,
+    },
+    {
+      value: 'option2',
+      label: 'Option 2',
+      checked: selectedOption === 'option2',
+      onChange: handleOptionChange,
+    },
+    {
+      value: 'option3',
+      label: 'Option 3',
+      checked: selectedOption === 'option3',
+      onChange: handleOptionChange,
+    },
+  ];
+
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div>
-        <RadioButtons />
+        <RadioButtons
+          options={options}
+          selectedValue={selectedOption}
+          onChange={handleOptionChange}
+        />
       </div>
     </main>
   );
--- src/components/radio.tsx
+++ src/components/radio.tsx
@@ -1,55 +1,68 @@
-'use client';
-import React, { useState } from 'react';
+import React from 'react';
 
-type RadioButtonValue = 'option1' | 'option2' | 'option3';
+interface RadioButtonProps {
+  value: string;
+  label: string;
+  checked: boolean;
+  disabled?: boolean;
+  onChange: (value: string) => void;
+}
 
-const RadioButtons = () => {
-  const [selectedOption, setSelectedOption] = useState<RadioButtonValue | null>(
-    null,
-  );
-
-  const handleOptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSelectedOption(e.target.value as RadioButtonValue);
+const RadioButton: React.FC<RadioButtonProps> = ({
+  value,
+  label,
+  checked,
+  disabled,
+  onChange,
+}) => {
+  const handleOptionChange = () => {
+    onChange(value);
   };
 
   return (
+    <label
+      className={`text-white inline-flex items-center ${
+        disabled ? 'opacity-50' : ''
+      }`}
+    >
+      <input
+        type='radio'
+        className='appearance-none w-[17px] h-[17px] border-solid border-2 rounded-[12px]	form-radio flex items-center justify-center outline-none
+        hover:border-[#dbb88b] after:content-[""] after:w-full after:h-full after:hidden after:bg-[url("/checked.svg")] after:bg-no-repeat after:bg-center
+        checked:after:block checked:border-solid checked:rounded-lg checked:border-0 checked:border-[#dbb88b]'
+        value={value}
+        checked={checked}
+        onChange={handleOptionChange}
+        disabled={disabled}
+      />
+      <span className='ml-2'>{label}</span>
+    </label>
+  );
+};
+
+interface RadioButtonsProps {
+  options: RadioButtonProps[];
+  selectedValue: string | null;
+  onChange: (value: string) => void;
+}
+
+const RadioButtons: React.FC<RadioButtonsProps> = ({
+  options,
+  selectedValue,
+  onChange,
+}) => {
+  return (
     <div className='bg-black mt-2 flex flex-col'>
-      <label className='text-white inline-flex items-center'>
-        <input
-          type='radio'
-          className='appearance-none w-[17px] h-[17px] border-solid border-2 rounded-[12px]	form-radio flex items-center justify-center outline-none
-          hover:border-[#dbb88b] after:content-[""] after:w-full after:h-full after:hidden after:bg-[url("/checked.svg")] after:bg-no-repeat after:bg-center
-          checked:after:block checked:border-solid checked:rounded-lg checked:border-0 checked:border-[#dbb88b]'
-          value='option1'
-          checked={selectedOption === 'option1'}
-          onChange={handleOptionChange}
+      {options.map((option) => (
+        <RadioButton
+          key={option.value}
+          value={option.value}
+          label={option.label}
+          checked={option.value === selectedValue}
+          onChange={onChange}
+          disabled={option.disabled || false}
         />
-        <span className='ml-2'>Option 1</span>
-      </label>
-      <label className='text-white inline-flex items-center mt-2'>
-        <input
-          type='radio'
-          className='appearance-none w-[17px] h-[17px] border-solid border-2 rounded-[12px]	form-radio
-          hover:border-[#dbb88b] after:content-[""] after:w-full after:h-full after:hidden after:bg-[url("/checked.svg")] after:bg-no-repeat after:bg-center
-          checked:after:block checked:border-solid checked:rounded-lg checked:border-0 checked:border-[#dbb88b]'
-          value='option2'
-          checked={selectedOption === 'option2'}
-          onChange={handleOptionChange}
-        />
-        <span className='ml-2'>Option 2</span>
-      </label>
-      <label className='text-white inline-flex items-center mt-2'>
-        <input
-          type='radio'
-          className='appearance-none w-[17px] h-[17px] border-solid border-2 rounded-[12px]	form-radio
-          hover:border-[#dbb88b] after:content-[""] after:w-full after:h-full after:hidden after:bg-[url("/checked.svg")] after:bg-no-repeat after:bg-center
-          checked:after:block checked:border-solid checked:rounded-lg checked:border-0 checked:border-[#dbb88b]'
-          value='option3'
-          checked={selectedOption === 'option3'}
-          onChange={handleOptionChange}
-        />
-        <span className='ml-2'>Option 3</span>
-      </label>
+      ))}
     </div>
   );
 };
