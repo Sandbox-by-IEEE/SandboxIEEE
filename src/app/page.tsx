a9b7bf7edc948f8c246f2f05afdd6a10bd046a37
Moh. Fairuz Alauddin Yahya <fairuzy210@gmail.com>
2023-09-16 11:46:26 +0700

    feat: Creating a dropdown component with 3 color variations as props and custom scrollbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,70 @@
-import dynamic from 'next/dynamic';
+'use client';
+import React, { useState } from 'react';
 
-const Countdown = dynamic(() => import('@/components/Countdown'), {
-  ssr: false,
-});
+import Dropdown from '@/components/Dropdown';
 
 export default function Home() {
+  // Dummy data for demonstration
+  const data = [
+    {
+      name: 'John Doe',
+      division: 'Resource Management',
+    },
+    {
+      name: 'Jane Smith',
+      division: 'Marketing',
+    },
+    {
+      name: 'Bob Johnson',
+      division: 'Web Development',
+    },
+    // Add more data items as needed
+  ];
+
+  const options = [
+    'Resource Management',
+    'Resource Development',
+    'Marketing',
+    'Partnership',
+    'Competition',
+    'Project',
+    'Paper',
+    'Event',
+    'Logistic',
+    'Web Development',
+    'Finance',
+    'Sponsorship',
+  ];
+
+  // State for the selected option in the dropdown
+  const [selectedOption, setSelectedOption] = useState<string>('');
+
   return (
-    <main className='flex min-h-screen w-full bg-white flex-col items-baseline justify-center'>
-      <div className='w-full flex items-center justify-center'>
-        <Countdown targetDate={new Date('2023-12-22')} />
+    <main className='flex min-h-screen w-full bg-red-500 flex-col items-baseline justify-center'>
+      <div className='w-full flex items-center justify-center gap-10'>
+        <Dropdown
+          color='green'
+          options={options}
+          placeholder='Division'
+          setSelectedOption={setSelectedOption}
+          selectedOption={selectedOption}
+        />
+        <Dropdown
+          color='light'
+          options={options}
+          placeholder='Division'
+          setSelectedOption={setSelectedOption}
+          selectedOption={selectedOption}
+        />
+        <Dropdown
+          color='trans-green'
+          options={options}
+          placeholder='Division'
+          setSelectedOption={setSelectedOption}
+          selectedOption={selectedOption}
+        />
       </div>
+      {/* You can map through the filteredData to display the results */}
     </main>
   );
 }
