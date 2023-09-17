6fe1e7b935e23ea7c9a0a8eecd56602b222aea33
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-09-17 21:01:12 +0700

    Card (#13)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,84 @@
 'use client';
 
-import { callToast } from '@/components/Toast';
+import Cards from '@/components/Card';
 
-const status = 'error';
-const desc = 'Pokoknya ada masalah lah pas lagi fetch datanya.';
+export default function Home() {
+  const cardData = [
+    {
+      title: 'Sample Card 1',
+      imageUrl: '/google-logo.png', // Replace with the actual image path
+      children:
+        'Lorem ipsum dolor sit ameta siudahsok dhjahsdljks hasjkdahsgdjkas hdjkas hdjka dhjkah sdjkh ...',
+      leftTag: 'Tag1',
+      rightTag: 'Tag2',
+      horizontal: false,
+      buttonText: 'Click Me',
+      onClick: () => {
+        // Handle button click
+      },
+    },
+    {
+      title: 'Sample Card 2',
+      imageUrl: '/google-logo.png', // Replace with the actual image path
+      children:
+        'Another card with different content dasjhd kjahsd jka hsdjkha sjdh ajkshd jkashdjk a hdkjash djkas hdjkhas djaksdhjk ashd jk...',
+      leftTag: 'Tag3',
+      rightTag: 'Tag4',
+      horizontal: true,
+      buttonText: 'Explore',
+      onClick: () => {
+        // Handle button click
+      },
+    },
+    {
+      title: 'Sample Card 1',
+      imageUrl: '/google-logo.png', // Replace with the actual image path
+      children:
+        'Lorem ipsum dolor sit ameta siudahsok dhjahsdljks hasjkdahsgdjkas hdjkas hdjka dhjkah sdjkh ...',
+      leftTag: 'Tag1',
+      rightTag: 'Tag2',
+      horizontal: false,
+      onClick: () => {
+        // Handle button click
+      },
+    },
+    {
+      title: 'Sample Card 2',
+      imageUrl: '/google-logo.png', // Replace with the actual image path
+      children:
+        'Another card with different content dasjhd kjahsd jka hsdjkha sjdh ajkshd jkashdjk a hdkjash djkas hdjkhas djaksdhjk ashd jk...',
+      leftTag: 'Tag3',
+      rightTag: 'Tag4',
+      horizontal: true,
+      onClick: () => {
+        // Handle button click
+      },
+    },
+    {
+      title: 'Sample Card 2',
+      imageUrl: '/google-logo.png', // Replace with the actual image path
+      children:
+        'Another card with different content dasjhd kjahsd jka hsdjkha sjdh ajkshd jkashdjk a hdkjash djkas hdjkhas djaksdhjk ashd jk...',
+      leftTag: 'Tag3',
+      rightTag: 'Tag4',
+      horizontal: false,
+      buttonText: 'yuhu',
+      onClick: () => {
+        // Handle button click
+      },
+    },
 
-export default function Page() {
+    // Add more card data objects as needed
+  ];
+
   return (
-    <div className='flex gap-4'>
-      <button
-        onClick={() => callToast({ status, description: desc })}
-        className='bg-red-300'
-      >
-        Make an error-type toast
-      </button>
-      <button
-        onClick={() =>
-          callToast({
-            status: 'success',
-            description: 'Ganti password berhasil.',
-          })
-        }
-        className='bg-green-300'
-      >
-        Make a success-type toast
-      </button>
-    </div>
+    <main className='flex min-h-screen w-full bg-red-500 flex-col items-baseline justify-center px-10 py-20'>
+      <div className='w-full flex items-center justify-center gap-10 flex-wrap'>
+        {cardData.map((data, index) => (
+          <Cards key={index} {...data} />
+        ))}
+      </div>
+      {/* You can map through the filteredData to display the results */}
+    </main>
   );
 }
