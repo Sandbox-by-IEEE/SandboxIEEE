d17645828d8b2c409b13291c6c1afd311072c26b
Moh. Fairuz Alauddin Yahya <fairuzy210@gmail.com>
2023-09-26 15:42:22 +0700

    refactor: Change props types, implements color different, and improve code efficiency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,401 +1,64 @@
+import React from 'react';
+
 import Button from '@/components/Button';
-import IconButton from '@/components/IconButton';
 
 export default function Home() {
-  const cardData = [
-    {
-      title: 'Sample Card 1',
-      imageUrl: '/google-logo.png', // Replace with the actual image path
-      children:
-        'Lorem ipsum dolor sit ameta siudahsok dhjahsdljks hasjkdahsgdjkas hdjkas hdjka dhjkah sdjkh ...',
-      leftTag: 'Tag1',
-      rightTag: 'Tag2',
-      horizontal: false,
-      buttonText: 'Click Me',
-      onClick: () => {
-        // Handle button click
-      },
-    },
-    {
-      title: 'Sample Card 2',
-      imageUrl: '/google-logo.png', // Replace with the actual image path
-      children:
-        'Another card with different content dasjhd kjahsd jka hsdjkha sjdh ajkshd jkashdjk a hdkjash djkas hdjkhas djaksdhjk ashd jk...',
-      leftTag: 'Tag3',
-      rightTag: 'Tag4',
-      horizontal: true,
-      buttonText: 'Explore',
-      onClick: () => {
-        // Handle button click
-      },
-    },
-    {
-      title: 'Sample Card 1',
-      imageUrl: '/google-logo.png', // Replace with the actual image path
-      children:
-        'Lorem ipsum dolor sit ameta siudahsok dhjahsdljks hasjkdahsgdjkas hdjkas hdjka dhjkah sdjkh ...',
-      leftTag: 'Tag1',
-      rightTag: 'Tag2',
-      horizontal: false,
-      onClick: () => {
-        // Handle button click
-      },
-    },
-    {
-      title: 'Sample Card 2',
-      imageUrl: '/google-logo.png', // Replace with the actual image path
-      children:
-        'Another card with different content dasjhd kjahsd jka hsdjkha sjdh ajkshd jkashdjk a hdkjash djkas hdjkhas djaksdhjk ashd jk...',
-      leftTag: 'Tag3',
-      rightTag: 'Tag4',
-      horizontal: true,
-      onClick: () => {
-        // Handle button click
-      },
-    },
-    {
-      title: 'Sample Card 2',
-      imageUrl: '/google-logo.png', // Replace with the actual image path
-      children:
-        'Another card with different content dasjhd kjahsd jka hsdjkha sjdh ajkshd jkashdjk a hdkjash djkas hdjkhas djaksdhjk ashd jk...',
-      leftTag: 'Tag3',
-      rightTag: 'Tag4',
-      horizontal: false,
-      buttonText: 'yuhu',
-      onClick: () => {
-        // Handle button click
-      },
-    },
-
-    // Add more card data objects as needed
-  ];
+  // ... (other code)
 
   return (
-    <main>
-      <div className='w-screen h-screen p-10 mx-auto space-x-2 space-y-3'>
-        <IconButton bgColor='green' />
-        <IconButton bgColor='gold' />
-        <IconButton bgColor='black' />
-        <IconButton bgColor='disabled' />
+    <main className='flex flex-col w-screen h-screen'>
+      <div className=' p-10 mx-auto space-x-2 space-y-3 w-[400px]'>
+        {/* Sample usage of the Button component */}
 
-        <Button //GREEN, 100%, !GLOW, !ARROW
-          isTextWhite={true}
-          bgColor='green'
-          bgOpacity={100}
-          isGlow={true}
-          text='Button'
-          isIcon={false}
-          isDisabled={false}
-        />
+        {/* GREEN, 100%, GLOW */}
+        <Button color='green' isIcon={false} isDisabled={false}>
+          Sample Button
+        </Button>
 
-        <Button //GREEN, 100%, GLOW, !ARROW
-          isTextWhite={true}
-          bgColor='green'
-          bgOpacity={100}
-          isGlow={false}
-          text='Button'
-          isIcon={false}
-          isDisabled={false}
-        />
+        {/* GREEN, 100%, NO GLOW */}
+        <Button color='trans-green' isIcon={false} isDisabled={false}>
+          Sample Button
+        </Button>
 
-        <Button //BLACK, 100%, !GLOW, !ARROW
-          isTextWhite={true}
-          bgColor='black'
-          bgOpacity={100}
-          isGlow={false}
-          text='Button'
-          isIcon={false}
-          isDisabled={false}
-        />
+        {/* BLACK, 100%, NO GLOW */}
+        <Button color='black' isIcon={false} isDisabled={true}>
+          Sample Button
+        </Button>
 
-        <Button //DISABLED, !ARROW
-          isTextWhite={true}
-          bgColor='disabled'
-          bgOpacity={100}
-          isGlow={false}
-          text='Button'
-          isIcon={false}
-          isDisabled={true}
-        />
+        {/* DISABLED */}
 
-        <Button //GOLD, 100%, GLOW, !ARROW
-          isTextWhite={true}
-          bgColor='gold'
-          bgOpacity={100}
-          isGlow={true}
-          text='Button'
-          isIcon={false}
-          isDisabled={false}
-        />
+        {/* GOLD, 100%, GLOW */}
 
-        <Button //GOLD, 100%, !GLOW, !ARROW
-          isTextWhite={true}
-          bgColor='gold'
-          bgOpacity={100}
-          isGlow={false}
-          text='Button'
-          isIcon={false}
-          isDisabled={false}
-        />
+        {/* GOLD, 100%, NO GLOW */}
+        <Button color='gold' isIcon={false} isDisabled={false}>
+          Sample Button
+        </Button>
 
-        <Button //BLACK, 100%, !GLOW, !ARROW
-          isTextWhite={true}
-          bgColor='black'
-          bgOpacity={100}
-          isGlow={false}
-          text='Button'
-          isIcon={false}
-          isDisabled={false}
-        />
+        {/* BLACK, 100%, NO GLOW */}
+        <Button color='black' isIcon={false} isDisabled={false}>
+          Sample Button
+        </Button>
 
-        <Button //DISABLED, !ARROW
-          isTextWhite={true}
-          bgColor='disabled'
-          bgOpacity={100}
-          isGlow={false}
-          text='Button'
-          isIcon={false}
-          isDisabled={true}
-        />
+        {/* DISABLED */}
 
-        <Button //GREEN, 100%, GLOW, ARROW
-          isTextWhite={true}
-          bgColor='green'
-          bgOpacity={100}
-          isGlow={true}
-          text='Button'
-          isIcon={true}
-          isDisabled={false}
-        />
+        {/* GREEN, 100%, GLOW, ARROW */}
+        <Button color='green' isIcon={true} isDisabled={false}>
+          Sample Button
+        </Button>
 
-        <Button //GREEN, 100%, !GLOW, ARROW
-          isTextWhite={true}
-          bgColor='green'
-          bgOpacity={100}
-          isGlow={false}
-          text='Button'
-          isIcon={true}
-          isDisabled={false}
-        />
+        {/* GREEN, 100%, NO GLOW, ARROW */}
+        <Button color='trans-orange' isIcon={true} isDisabled={false}>
+          Sample Button
+        </Button>
 
-        <Button //BLACK, 100%, !GLOW, ARROW
-          isTextWhite={true}
-          bgColor='black'
-          bgOpacity={100}
-          isGlow={false}
-          text='Button'
-          isIcon={true}
-          isDisabled={false}
-        />
+        {/* BLACK, 100%, NO GLOW, ARROW */}
+        <Button color='black' isIcon={true} isDisabled={false}>
+          Sample Button
+        </Button>
 
-        <Button //DISABLED, ARROW
-          isTextWhite={true}
-          bgColor='disabled'
-          bgOpacity={100}
-          isGlow={false}
-          text='Button'
-          isIcon={true}
-          isDisabled={true}
-        />
+        {/* DISABLED, ARROW */}
 
-        <Button //GOLD, 100%, GLOW, ARROW
-          isTextWhite={true}
-          bgColor='gold'
-          bgOpacity={100}
-          isGlow={true}
-          text='Button'
-          isIcon={true}
-          isDisabled={false}
-        />
-
-        <Button //GOLD, 100%, GLOW, ARROW
-          isTextWhite={true}
-          bgColor='gold'
-          bgOpacity={100}
-          isGlow={false}
-          text='Button'
-          isIcon={true}
-          isDisabled={false}
-        />
-
-        <Button //BLACK, 100%, !GLOW, ARROW
-          isTextWhite={true}
-          bgColor='black'
-          bgOpacity={100}
-          isGlow={false}
-          text='Button'
-          isIcon={true}
-          isDisabled={false}
-        />
-
-        <Button //DISABLED, ARROW
-          isTextWhite={true}
-          bgColor='disabled'
-          bgOpacity={100}
-          isGlow={false}
-          text='Button'
-          isIcon={true}
-          isDisabled={true}
-        />
-
-        <Button //GREEN, 0%, !GLOW, !ARROW
-          isTextWhite={false}
-          bgColor='green'
-          bgOpacity={0}
-          isGlow={false}
-          text='Button'
-          isIcon={false}
-          isDisabled={false}
-        />
-
-        <Button //GREEN, 20%, !GLOW, !ARROW
-          isTextWhite={false}
-          bgColor='green'
-          bgOpacity={20}
-          isGlow={false}
-          text='Button'
-          isIcon={false}
-          isDisabled={false}
-        />
-
-        <Button //wGREEN, 100%, !GLOW, !ARROW
-          isTextWhite={false}
-          bgColor='green'
-          bgOpacity={100}
-          isGlow={false}
-          text='Button'
-          isIcon={false}
-          isDisabled={false}
-        />
-
-        <Button //DISABLED, !ARROW
-          isTextWhite={true}
-          bgColor='disabled'
-          bgOpacity={100}
-          isGlow={false}
-          text='Button'
-          isIcon={false}
-          isDisabled={true}
-        />
-
-        <Button //GOLD, 0%, !GLOW, !ARROW
-          isTextWhite={false}
-          bgColor='gold'
-          bgOpacity={0}
-          isGlow={false}
-          text='Button'
-          isIcon={false}
-          isDisabled={false}
-        />
-
-        <Button //GOLD, 20%, !GLOW, !ARROW
-          isTextWhite={false}
-          bgColor='gold'
-          bgOpacity={20}
-          isGlow={false}
-          text='Button'
-          isIcon={false}
-          isDisabled={false}
-        />
-
-        <Button //wGOLD, 100%, !GLOW, !ARROW
-          isTextWhite={false}
-          bgColor='gold'
-          bgOpacity={100}
-          isGlow={false}
-          text='Button'
-          isIcon={false}
-          isDisabled={false}
-        />
-
-        <Button //DISABLED, !ARROW
-          isTextWhite={true}
-          bgColor='disabled'
-          bgOpacity={100}
-          isGlow={false}
-          text='Button'
-          isIcon={false}
-          isDisabled={true}
-        />
-        <Button //GREEN, 0%, !GLOW, ARROW
-          isTextWhite={false}
-          bgColor='green'
-          bgOpacity={0}
-          isGlow={false}
-          text='Button'
-          isIcon={true}
-          isDisabled={false}
-        />
-
-        <Button //GREEN, 20%, !GLOW, ARROW
-          isTextWhite={false}
-          bgColor='green'
-          bgOpacity={20}
-          isGlow={false}
-          text='Button'
-          isIcon={true}
-          isDisabled={false}
-        />
-
-        <Button //wGREEN, 100%, !GLOW, ARROW
-          isTextWhite={false}
-          bgColor='green'
-          bgOpacity={100}
-          isGlow={false}
-          text='Button'
-          isIcon={true}
-          isDisabled={false}
-        />
-
-        <Button //DISABLED, !ARROW
-          isTextWhite={true}
-          bgColor='disabled'
-          bgOpacity={100}
-          isGlow={false}
-          text='Button'
-          isIcon={true}
-          isDisabled={true}
-        />
-
-        <Button //GOLD, 0%, !GLOW, ARROW
-          isTextWhite={false}
-          bgColor='gold'
-          bgOpacity={0}
-          isGlow={false}
-          text='Button'
-          isIcon={true}
-          isDisabled={false}
-        />
-
-        <Button //GOLD, 20%, !GLOW, ARROW
-          isTextWhite={false}
-          bgColor='gold'
-          bgOpacity={20}
-          isGlow={false}
-          text='Button'
-          isIcon={true}
-          isDisabled={false}
-        />
-
-        <Button //wGOLD, 100%, !GLOW, ARROW
-          isTextWhite={false}
-          bgColor='gold'
-          bgOpacity={100}
-          isGlow={false}
-          text='Button'
-          isIcon={true}
-          isDisabled={false}
-        />
-
-        <Button //DISABLED, ARROW
-          isTextWhite={true}
-          bgColor='disabled'
-          bgOpacity={100}
-          isGlow={false}
-          text='Button'
-          isIcon={true}
-          isDisabled={true}
-        />
+        {/* Continue adding more button examples as needed */}
       </div>
       {/* You can map through the filteredData to display the results */}
     </main>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,324 +1,47 @@
 import RightArrow from './icons/RightArrow';
 
 const Button = ({
-  isTextWhite,
-  bgColor,
-  bgOpacity,
-  isGlow,
-  text = 'Button',
+  color,
+  children,
   isIcon,
   isDisabled,
+  isFullWidth,
 }: {
-  isTextWhite?: boolean;
-  bgColor: 'green' | 'gold' | 'black' | 'disabled';
-  bgOpacity: number;
-  isGlow?: boolean;
-  text?: string | undefined;
+  color: 'green' | 'gold' | 'black' | 'trans-green' | 'trans-orange';
+  children: JSX.Element | string;
   isIcon?: boolean;
   isDisabled?: boolean;
+  isFullWidth?: boolean;
 }) => {
-  //disabled
-  if (isDisabled) {
-    return (
-      <button
-        className={
-          isIcon
-            ? 'relative sm:w-[200px] sm:h-[48px] w-[110px] h-[35px]'
-            : 'relative sm:w-[185px] sm:h-[48px] w-[105px] h-[35px]'
-        }
-      >
-        <div className='absolute inset-0 bg-[#D7D2D0] flex justify-center items-center w-full h-full rounded-md cursor-not-allowed'>
-          <div className='flex gap-2'>
-            <p
-              className={
-                isTextWhite
-                  ? 'text-white font-bold'
-                  : 'text-[#0D432F] font-bold'
-              }
-            >
-              {text}
-            </p>
-            {isIcon && <RightArrow arrowColor='#FFFFFF' />}
-          </div>
-        </div>
-      </button>
-    );
-  }
+  const colorEffect = {
+    green:
+      'bg-[#0D432F] hover:bg-[#315B4C] text-[#0D432F] hover:shadow-[0px_0px_20px_5px_#315B4C] text-white',
+    black:
+      'bg-[#1C1A17] text-[#0D432F] text-white hover:shadow-[0px_0px_3px_3px_#FFFFFF]',
+    gold: 'bg-[#AB814E] hover:bg-[#B49876] rounded-md hover:shadow-[0px_0px_20px_5px_#B49876]  text-white',
+    'trans-green':
+      'border border-[#0D432F] bg-transparent text-[#0D432F] hover:bg-[#494845]',
+    'trans-orange':
+      'border border-[#AB814E] bg-transparent text-[#AB814E] hover:bg-[#494845]',
+  };
 
   //green, 100%
-  if (bgOpacity === 100 && bgColor === 'green' && isTextWhite) {
-    return (
-      <button
-        className={
-          isIcon
-            ? 'relative sm:w-[200px] sm:h-[48px] w-[110px] h-[35px]'
-            : 'relative sm:w-[185px] sm:h-[48px] w-[105px] h-[35px]'
-        }
-      >
-        <div className='absolute inset-0 bg-[#0D432F] hover:bg-[#315B4C] flex justify-center items-center w-full h-full rounded-md shadow-[0px_0px_20px_5px_#315B4C] hover:shadow-md'>
-          <div className='flex gap-2'>
-            <p
-              className={
-                isTextWhite
-                  ? 'text-white font-bold'
-                  : 'text-[#0D432F] font-bold'
-              }
-            >
-              {text}
-            </p>
-            {isIcon && <RightArrow arrowColor='#FFFFFF' />}
-          </div>
-        </div>
-      </button>
-    );
-  }
-
-  //gold, 100%
-  if (bgOpacity === 100 && bgColor === 'gold' && isTextWhite) {
-    return (
-      <button
-        className={
-          isIcon
-            ? 'relative sm:w-[200px] sm:h-[48px] w-[110px] h-[35px]'
-            : 'relative sm:w-[185px] sm:h-[48px] w-[105px] h-[35px]'
-        }
-      >
-        <div className='absolute inset-0 bg-[#AB814E] hover:bg-[#B49876] flex justify-center items-center w-full h-full rounded-md shadow-[0px_0px_20px_5px_#B49876] hover:shadow-md '>
-          <div className='flex gap-2'>
-            <p
-              className={
-                isTextWhite
-                  ? 'text-white font-bold'
-                  : 'text-[#0D432F] font-bold'
-              }
-            >
-              {text}
-            </p>
-            {isIcon && <RightArrow arrowColor='#FFFFFF' />}
-          </div>
-        </div>
-      </button>
-    );
-  }
-
-  //black, 100%
-  if (bgOpacity === 100 && bgColor === 'black' && isTextWhite) {
-    return (
-      <button
-        className={
-          isIcon
-            ? 'relative sm:w-[200px] sm:h-[48px] w-[110px] h-[35px]'
-            : 'relative sm:w-[185px] sm:h-[48px] w-[105px] h-[35px]'
-        }
-      >
-        {isGlow && (
-          <div className='absolute inset-0 bg-[#1C1A17] flex justify-center items-center w-full h-full rounded-md blur' />
-        )}
-        <div className='absolute inset-0 bg-[#1C1A17] flex justify-center items-center w-full h-full rounded-md shadow-md '>
-          <div className='flex gap-2'>
-            <p
-              className={
-                isTextWhite
-                  ? 'text-white font-bold'
-                  : 'text-[#0D432F] font-bold'
-              }
-            >
-              {text}
-            </p>
-            {isIcon && <RightArrow arrowColor='#FFFFFF' />}
-          </div>
-        </div>
-      </button>
-    );
-  }
-
-  //green, white 0%
-  if (bgOpacity === 0 && bgColor === 'green' && !isTextWhite) {
-    return (
-      <button
-        className={
-          isIcon
-            ? 'relative sm:w-[200px] sm:h-[48px] w-[110px] h-[35px]'
-            : 'relative sm:w-[185px] sm:h-[48px] w-[105px] h-[35px]'
-        }
-      >
-        <div className='absolute inset-0 border-2 border-[#0D432F] bg-[#FFFFFF] flex justify-center items-center w-full h-full rounded-md hover:bg-[#FFFFFF] '>
-          <div className='flex gap-2'>
-            <p
-              className={
-                isTextWhite
-                  ? 'text-white font-bold'
-                  : 'text-[#0D432F] font-bold'
-              }
-            >
-              {text}
-            </p>
-            {isIcon && <RightArrow arrowColor='#0D432F' />}
-          </div>
-        </div>
-      </button>
-    );
-  }
-
-  //gold, white 0%
-  if (bgOpacity === 0 && bgColor === 'gold' && !isTextWhite) {
-    return (
-      <button
-        className={
-          isIcon
-            ? 'relative sm:w-[200px] sm:h-[48px] w-[110px] h-[35px]'
-            : 'relative sm:w-[185px] sm:h-[48px] w-[105px] h-[35px]'
-        }
-      >
-        <div className='absolute inset-0 border-2 border-[#AB814E] bg-[ffffff00] flex justify-center items-center w-full h-full rounded-md '>
-          <div className='flex gap-2'>
-            <p
-              className={
-                isTextWhite
-                  ? 'text-white font-bold'
-                  : 'text-[#AB814E] font-bold'
-              }
-            >
-              {text}
-            </p>
-            {isIcon && <RightArrow arrowColor='#AB814E' />}
-          </div>
-        </div>
-      </button>
-    );
-  }
-  //green, white 20%
-  if (bgOpacity === 20 && bgColor === 'green' && !isTextWhite) {
-    return (
-      <button
-        className={
-          isIcon
-            ? 'relative sm:w-[200px] sm:h-[48px] w-[110px] h-[35px]'
-            : 'relative sm:w-[185px] sm:h-[48px] w-[105px] h-[35px]'
-        }
-      >
-        <div className='absolute inset-0 border-2 bg-[#FFFFFF] opacity-20 flex justify-center items-center w-full h-full rounded-md '></div>
-        <div className='absolute inset-0 border-2 border-[#0D432F] bg-[ffffff00] flex justify-center items-center w-full h-full rounded-md '>
-          <div className='flex gap-2'>
-            <p
-              className={
-                isTextWhite
-                  ? 'text-white font-bold'
-                  : 'text-[#0D432F] font-bold'
-              }
-            >
-              {text}
-            </p>
-            {isIcon && <RightArrow arrowColor='#0D432F' />}
-          </div>
-        </div>
-      </button>
-    );
-  }
-
-  //gold, white 20%
-  if (bgOpacity === 20 && bgColor === 'gold' && !isTextWhite) {
-    return (
-      <button
-        className={
-          isIcon
-            ? 'relative sm:w-[200px] sm:h-[48px] w-[110px] h-[35px]'
-            : 'relative sm:w-[185px] sm:h-[48px] w-[105px] h-[35px]'
-        }
-      >
-        <div className='absolute inset-0 border-2 bg-[#FFFFFF] opacity-20 flex justify-center items-center w-full h-full rounded-md '></div>
-        <div className='absolute inset-0 border-2 border-[#AB814E] bg-[ffffff00] flex justify-center items-center w-full h-full rounded-md '>
-          <div className='flex gap-2'>
-            <p
-              className={
-                isTextWhite
-                  ? 'text-white font-bold'
-                  : 'text-[#AB814E] font-bold'
-              }
-            >
-              {text}
-            </p>
-            {isIcon && <RightArrow arrowColor='#AB814E' />}
-          </div>
-        </div>
-      </button>
-    );
-  }
-
-  //green, white 100%
-  if (bgOpacity === 100 && bgColor === 'green' && !isTextWhite) {
-    return (
-      <button
-        className={
-          isIcon
-            ? 'relative sm:w-[200px] sm:h-[48px] w-[110px] h-[35px]'
-            : 'relative sm:w-[185px] sm:h-[48px] w-[105px] h-[35px]'
-        }
-      >
-        <div className='absolute inset-0 border-2 border-[#0D432F] bg-[#FFFFFF] flex justify-center items-center w-full h-full rounded-md '>
-          <div className='flex gap-2'>
-            <p
-              className={
-                isTextWhite
-                  ? 'text-white font-bold'
-                  : 'text-[#0D432F] font-bold'
-              }
-            >
-              {text}
-            </p>
-            {isIcon && <RightArrow arrowColor='#0D432F' />}
-          </div>
-        </div>
-      </button>
-    );
-  }
-
-  //gold, white 100%
-  if (bgOpacity === 100 && bgColor === 'gold') {
-    return (
-      <button
-        className={
-          isIcon
-            ? 'relative sm:w-[200px] sm:h-[48px] w-[110px] h-[35px]'
-            : 'relative sm:w-[185px] sm:h-[48px] w-[105px] h-[35px]'
-        }
-      >
-        <div className='absolute inset-0 border-2 border-[#AB814E] bg-[ffffff00] flex justify-center items-center w-full h-full rounded-md '>
-          <div className='flex gap-2'>
-            <p
-              className={
-                isTextWhite
-                  ? 'text-white font-bold'
-                  : 'text-[#AB814E] font-bold'
-              }
-            >
-              {text}
-            </p>
-            {isIcon && <RightArrow arrowColor='#AB814E' />}
-          </div>
-        </div>
-      </button>
-    );
-  }
-
-  //test button
   return (
-    <button className='relative w-[185px] h-[48px] sm:w-[110px] sm:h-[35px]'>
-      <div className='absolute inset-0 bg-[#4c7fcd] flex justify-center items-center w-full h-full rounded-md blur' />
-      <div className='absolute inset-0 bg-[#4c7fcd] flex justify-center items-center w-full h-full rounded-md '>
-        <div className='flex gap-2'>
-          <p
-            className={
-              isTextWhite ? 'text-white font-bold' : 'text-green-500 font-bold'
-            }
-          >
-            Choose the correct styling
-          </p>
-          {isIcon && <RightArrow arrowColor='#D7D2D0' />}
-        </div>
-      </div>
+    <button
+      disabled={isDisabled}
+      className={`${
+        isFullWidth ? 'w-full' : 'w-[130px] lg:w-[200px]'
+      } text-sm lg:text-base font-bold disabled:bg-[#D7D2D0] disabled:cursor-not-allowed disabled:text-white font-poppins h-fit transition-all duration-300 flex justify-center items-center py-2 px-3 lg:py-3 lg:px-4 hover:shadow-md rounded-md ${
+        colorEffect[color]
+      }`}
+    >
+      <p className='flex gap-3 w-full items-center justify-center'>
+        {children}
+        {isIcon && <RightArrow arrowColor='#FFFFFF' />}
+      </p>
     </button>
   );
+  //test button
 };
 
 export default Button;
