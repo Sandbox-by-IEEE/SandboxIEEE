6b9da7cce2ece69a2832f8ef5eeb42f124816b69
Alvin Christopher Santausa <alvinchrisantausa@gmail.com>
2023-09-27 13:36:14 +0700

    style: bigger font size, use font poppins, hidden on mobile

--- src/components/breadcrumps.tsx
+++ src/components/breadcrumps.tsx
@@ -11,7 +11,7 @@ const Breadcrumbs = () => {
   }, []);
 
   return (
-    <nav className='hidden transition-all duration-1000 sm:block text-xl font-poppins'>
+    <nav className='hidden transition-all duration-300 sm:block text-xl font-poppins'>
       <ol className='flex'>
         <li>
           <Link href='/'>
