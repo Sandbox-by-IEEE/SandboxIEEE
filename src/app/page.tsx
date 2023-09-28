28d2ecc784cf6035d606154693e93d27a82110f8
Naufarrel <105210247+reletz@users.noreply.github.com>
2023-09-28 09:50:18 +0700

    [DONE] Button (#3)
    * feat: add buttons
    
    * style: added new Icon Button and new styles for the hover animation
    
    * refactor: Change props types, implements color different, and improve code efficiency
    
    * refactor: easier to input the condition of the IconButton needed
    
    * style: Change color hovering issue on shadow
    
    ---------
    
    Co-authored-by: Moh. Fairuz Alauddin Yahya <fairuzy210@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,65 @@
-'use client';
-import { useContext } from 'react';
+import React from 'react';
 
-import Modal from '@/components/Modal/Modal';
-import {
-  ModalContext,
-  ModalContextContextType,
-} from '@/components/Modal/ModalContext';
+import Button from '@/components/Button';
+import IconButton from '@/components/IconButton';
 
 export default function Home() {
-  const { openModal, setOpenModal } =
-    useContext<ModalContextContextType>(ModalContext);
+  // ... (other code)
 
   return (
-    <>
-      <main className='flex min-h-screen flex-col items-center justify-center p-24 gap-4'>
-        {/* Moodal */}
-        {openModal && (
-          <Modal
-            title='Modals Title'
-            description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. In laoreet auctor viverra. Nulla facilisis elit ac leo ornare congue. Morbi sed lectus maximus, efficitur orci a.'
-          />
-        )}
-        <button
-          className='px-4 py-1 bg-green-50 rounded-md'
-          onClick={() => setOpenModal(true)}
-        >
-          Open Modal
-        </button>
-      </main>
-    </>
+    <main className='flex flex-col bg-black w-screen h-screen'>
+      <div className=' p-10 mx-auto space-y-3 w-[400px] '>
+        {/* Sample usage of the Button component */}
+
+        {/* GREEN, 100%, GLOW */}
+        <Button color='green' isIcon={false} isDisabled={false}>
+          Sample Button
+        </Button>
+
+        {/* GREEN, 100%, NO GLOW */}
+        <Button color='trans-green' isIcon={false} isDisabled={false}>
+          Sample Button
+        </Button>
+
+        {/* BLACK, 100%, NO GLOW */}
+        <Button color='black' isIcon={false} isDisabled={true}>
+          Sample Button
+        </Button>
+
+        {/* GOLD, 100%, GLOW */}
+
+        {/* GOLD, 100%, NO GLOW */}
+        <Button color='gold' isIcon={false} isDisabled={false}>
+          Sample Button
+        </Button>
+
+        {/* BLACK, 100%, NO GLOW */}
+        <Button color='black' isIcon={false} isDisabled={false}>
+          Sample Button
+        </Button>
+
+        {/* GREEN, 100%, GLOW, ARROW */}
+        <Button color='green' isIcon={true} isDisabled={false}>
+          Sample Button
+        </Button>
+
+        {/* GREEN, 100%, NO GLOW, ARROW */}
+        <Button color='trans-orange' isIcon={true} isDisabled={false}>
+          Sample Button
+        </Button>
+
+        {/* BLACK, 100%, NO GLOW, ARROW */}
+        <Button color='black' isIcon={true} isDisabled={false}>
+          Sample Button
+        </Button>
+
+        <IconButton color='black' />
+        <IconButton color='green' />
+        <IconButton color='gold' />
+        <IconButton color='black' isDisabled={true} />
+        {/* Continue adding more button examples as needed */}
+      </div>
+      {/* You can map through the filteredData to display the results */}
+    </main>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,58 @@
+import RightArrow from './icons/RightArrow';
+
+const Button = ({
+  color,
+  children,
+  isIcon,
+  isDisabled,
+  isFullWidth,
+}: {
+  color: 'green' | 'gold' | 'black' | 'trans-green' | 'trans-orange';
+  children: JSX.Element | string;
+  isIcon?: boolean;
+  isDisabled?: boolean;
+  isFullWidth?: boolean;
+}) => {
+  const colorEffect = {
+    green: {
+      main: 'bg-[#0D432F] hover:shadow-[0px_0px_20px_5px_#315B4C] text-white',
+      arrow: '#FFFFFF',
+    },
+    black: {
+      main: 'bg-[#1C1A17] text-[#0D432F] text-white hover:shadow-[0px_0px_3px_3px_#FFFFFF] hover:bg-[#494845]',
+      arrow: '#FFFFFF',
+    },
+    gold: {
+      main: 'bg-[#AB814E] rounded-md hover:shadow-[0px_0px_20px_5px_#B49876] text-white',
+      arrow: '#FFFFFF',
+    },
+    'trans-green': {
+      main: 'border border-[3px] border-[#0D432F] bg-transparent text-[#0D432F] hover:bg-[#494845]',
+      arrow: '#0D432F',
+    },
+    'trans-orange': {
+      main: 'border border-[3px] border-[#AB814E] bg-transparent text-[#AB814E] hover:bg-[#494845]',
+      arrow: '#AB814E',
+    },
+  };
+
+  //green, 100%
+  return (
+    <button
+      disabled={isDisabled}
+      className={`${
+        isFullWidth ? 'w-full' : 'w-[130px] lg:w-[200px]'
+      } text-sm lg:text-base disabled:bg-[#D7D2D0] disabled:cursor-not-allowed disabled:text-white h-fit disabled:shadow-sm transition-all duration-300 flex justify-center items-center py-2 px-3 lg:py-3 lg:px-4 rounded-md ${
+        colorEffect[color].main
+      }`}
+    >
+      <p className='flex gap-3 w-full items-center justify-center font-poppins font-bold'>
+        {children}
+        {isIcon && <RightArrow arrowColor={`${colorEffect[color].arrow}`} />}
+      </p>
+    </button>
+  );
+  //test button
+};
+
+export default Button;
