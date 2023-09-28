e10da86ee6285150487be9082b4ba737bf246046
Alvin Christopher <75166708+Incheon21@users.noreply.github.com>
2023-09-28 09:53:00 +0700

    [DONE] Breadcrumbs (#18)
    * feat: added breadcrumps
    
    * style: bigger font size, use font poppins, hidden on mobile
    
    * refactor: Delete unused page
    
    ---------
    
    Co-authored-by: Moh. Fairuz Alauddin Yahya <fairuzy210@gmail.com>

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import React from 'react';
 
 import Breadcrumbs from '@/components/Breadcrumbs';
 
@@ -7,6 +8,14 @@ export default function Home() {
     <main className='bg-black flex min-h-screen flex-col items-center justify-between p-24'>
       <div>
         <Breadcrumbs />
+        <a
+          className='mt-4'
+          href={'/contacts/managers'}
+          target=''
+          rel='noopener noreferrer'
+        >
+          <p className='text-white hover:underline mb-1'>new page</p>
+        </a>
       </div>
     </main>
   );
