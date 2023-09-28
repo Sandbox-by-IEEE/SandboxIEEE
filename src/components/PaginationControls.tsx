07746d4b3ad5167c16b6b3468359a8f5326dd035
Edogawawa <nipponlegend72@gmail.com>
2023-09-28 12:49:52 +0700

    ini Pagination

--- src/components/PaginationControls.tsx
+++ src/components/PaginationControls.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import { FC } from 'react';
+import { useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 
 interface PaginationControlProps {
   hasNextPage: boolean;
   hasPrevPage: boolean;
-  dataLength : number;
+  dataLength: number;
 }
 
 const PaginationControls: FC<PaginationControlProps> = ({
@@ -20,46 +21,59 @@ const PaginationControls: FC<PaginationControlProps> = ({
   const page = searchParams.get('page') ?? '1';
   const per_page = searchParams.get('per_page') ?? '5';
 
-  // const dataLength = 10;
+  const numbers: number[] = [];
+
+  // Using a for loop to generate numbers from 1 to 5
+  for (let i = 1; i <= 5; i++) {
+    numbers.push(i);
+  }
+
+  const [values, setValues] = useState(numbers);
+
+  const handleClickPlus = () => {
+    const addedValues = values.map((num) => num + 5);
+    setValues(addedValues);
+  };
+
+  const handleClickMinus = () => {
+    const addedValues = values.map((num) => num - 5);
+    setValues(addedValues);
+  };
 
   return (
     <div className='flex gap-2 px-2 py-2'>
       <button
-        className='bg-white text-black p-1 mx-1 my-1'
+        className='bg-white text-black p-1 mx-1 my-1 h-1/2'
         style={{ borderRadius: '50%' }}
         disabled={!hasPrevPage}
-        onClick={() => {
-          router.push(`/?page=${Number(page) - 1}&per_pages=${per_page}`);
-        }}
+        onClick={handleClickMinus}
       >
         <p className='font-bold text-xs'>&nbsp;&lt;&nbsp;</p>
       </button>
 
-      <div className='flex flex-row text-white'>
-        <p className='mx-5'>
-          {page} / {Math.ceil(Number(dataLength) / Number(per_page))}
-        </p>
-        <p className='mx-5'>
-          {page} / {Math.ceil(Number(dataLength) / Number(per_page))}
-        </p>
-        <p className='mx-5'>
-          {page} / {Math.ceil(Number(dataLength) / Number(per_page))}
-        </p>
-        <p className='mx-5'>
-          {page} / {Math.ceil(Number(dataLength) / Number(per_page))}
-        </p>
-        <p className='mx-5'>
-          {page} / {Math.ceil(Number(dataLength) / Number(per_page))}
-        </p>
+      {/* if page == number */}
+      {/* page background ganti */}
+      <div className='flex text-white'>
+        {values.map((num, index) => (
+          <button
+            className='text-white p-1 mx-4 my-1 active:bg-green-900 rounded-full'
+            // disabled={!hasPrevPage}
+            onClick={() => {
+              router.push(`/?page=${num}&per_pages=${per_page}`);
+            }}
+          >
+            <p className='mx-2' key={num}>
+              {num}
+            </p>
+          </button>
+        ))}
       </div>
 
       <button
-        className='bg-white text-black p-1 mx-1 my-1'
+        className='bg-white text-black p-1 mx-1 my-1 h-1/2'
         style={{ borderRadius: '50%' }}
         disabled={!hasNextPage}
-        onClick={() => {
-          router.push(`/?page=${Number(page) + 1}&per_pages=${per_page}`);
-        }}
+        onClick={handleClickPlus}
       >
         <p className='font-bold text-xs'>&nbsp;&gt;&nbsp;</p>
       </button>
