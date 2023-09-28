13757fd2f99d2b8351cee685664c32d6d930c0d2
Moh. Fairuz Alauddin Yahya <fairuzy210@gmail.com>
2023-09-28 09:50:00 +0700

    refactor: Delete unused page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,6 @@ export default function Home() {
     <main className='bg-black flex min-h-screen flex-col items-center justify-between p-24'>
       <div>
         <Breadcrumbs />
-        <a
-          className='mt-4'
-          href={'/contacts'}
-          target=''
-          rel='noopener noreferrer'
-        >
-          <p className='text-white hover:underline mb-1'>new page</p>
-        </a>
       </div>
     </main>
   );
