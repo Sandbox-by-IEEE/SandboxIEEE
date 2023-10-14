e9d09d0738f1feec450a8a23c67537ff7bd5d02b
Alvin Christopher <75166708+Incheon21@users.noreply.github.com>
2023-10-14 16:45:56 +0700

    [DONE] Login page (#27)
    * feat: add login page (-text inpu)
    
    * feat: add textinput

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import Footer from '@/components/footer';
-
 export default function Home() {
   return (
-    <main className='bg-black flex min-h-screen flex-col items-center justify-between p-24'>
+    <main className='bg-black text-white flex min-h-screen flex-col items-center justify-between p-24'>
       <div>
-        <Footer />
+        <a className='hover:underline' href='/login'>
+          <h1>Login page</h1>
+        </a>
       </div>
       {/* You can map through the filteredData to display the results */}
     </main>
