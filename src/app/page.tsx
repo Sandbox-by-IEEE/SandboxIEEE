c1dea47d7f42e0f45d67c7dc2aacf052913e372c
Alvin Christopher <75166708+Incheon21@users.noreply.github.com>
2023-10-14 16:52:27 +0700

    [DONE] Registration page (#26)
    * feat: add registration page (-text input)
    
    * style: remove an unnecessary div
    
    * style: fix margin
    
    * [DONE] Login page (#27)
    
    * feat: add login page (-text inpu)
    
    * feat: add textinput
    
    ---------
    
    Co-authored-by: Moh. Fairuz Alauddin Yahya <fairuzy210@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main className='bg-black text-white flex min-h-screen flex-col items-center justify-between p-24'>
       <div>
-        <a className='hover:underline' href='/login'>
-          <h1>Login page</h1>
+        <a href={'../register'} target='' rel='noopener noreferrer'>
+          <p className='text-white hover:underline mb-1'>Register</p>
         </a>
       </div>
       {/* You can map through the filteredData to display the results */}
