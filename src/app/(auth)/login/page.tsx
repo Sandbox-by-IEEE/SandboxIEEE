fe7d5225ed175003f1909fd8922a1b29833b1465
Moh. Fairuz Alauddin Yahya <fairuzy210@gmail.com>
2023-10-15 20:26:28 +0700

    refactor: Refactor code on register and login page state inputs

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,9 @@
 'use client';
+// Importing necessary components and libraries from Next.js and React
 import Link from 'next/link';
 import React, { useState } from 'react';
 
+// Importing custom components for UI elements and icons
 import Button from '@/components/Button';
 import Google from '@/components/icons/Register/google';
 import Smalllogo from '@/components/icons/Register/Ieee';
@@ -14,52 +16,33 @@ import Stars3 from '@/components/icons/Register/stars3';
 import Stars3mb from '@/components/icons/Register/stars3mb';
 import TextInput from '@/components/TextInput';
 
+// Defining the functional component Home
 export default function Home() {
-  const [text, setText] = useState('');
+  // Using useState hook to manage the state for email, username, and password inputs
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  const InputColumn = ({ label, inputs }) => (
-    <div>
-      <p>{label}</p>
-      {inputs.map((input, index) => (
-        <TextInput
-          key={index}
-          text={text}
-          setText={setText}
-          color='white'
-          fullwidth={true}
-          type={input.type}
-          placeholder={input.placeholder}
-        />
-      ))}
-    </div>
-  );
-
-  const inputsData = [
-    [
-      {
-        label: 'Email',
-        inputs: [{ type: 'email', placeholder: 'Your email address' }],
-      },
-      {
-        label: 'Password',
-        inputs: [{ type: 'password', placeholder: 'Your password' }],
-      },
-    ],
-  ];
-
+  // The component returns the UI structure for the registration page
   return (
+    // Main container with a full height, a white background, and center-aligned items
     <main className='h-screen font-poppins text-sm sm:text-base flex bg-white flex-col items-center'>
+      {/* A container with a screen-wide height of 3000px to display the registration form */}
       <div className='w-screen h-[3000px] flex'>
+        {/* An empty div with a width of 0% on small screens and 50% on larger screens */}
         <div className='w-[0%] lg:w-[50%] justify-center z-10'></div>
-        <div className='relative w-[100%] lg:w-[50%] bg-[url("/assets/RegisterBackground.png")] bg-cover bg-no-repeat text-white bg-black items-center justify-center'>
+        {/* A div with a background image, covering the full width on small screens and 50% on larger screens */}
+        <div className='relative w-[100%] lg:w-[50%] bg-[url("/assets/RelogBackground.png")] bg-cover bg-no-repeat text-white bg-black items-center justify-center'>
+          {/* A wrapper div to contain various elements */}
           <div className='flex w-full h-full'>
+            {/* A logo at the top right corner */}
             <div className='absolute right-6 top-6 lg:right-10 lg:top-10'>
               <Smalllogo size={120} />
             </div>
+            {/* Stars icons to add a decorative element, displayed differently on small and larger screens */}
             <div className='hidden lg:block absolute'>
               <Stars1 size={17} />
             </div>
-            <div className='lg:hidden absolute'>
+            <div className='lg:hidden absolute top-2'>
               <Stars1mb size={22} />
             </div>
             <div className='hidden lg:block absolute bottom-0'>
@@ -74,40 +57,60 @@ export default function Home() {
             <div className='lg:hidden absolute bottom-0 right-0'>
               <Stars3mb size={25} />
             </div>
+            {/* A container to hold the registration form, with different padding on small and larger screens */}
             <div className='w-full overflow-hidden z-10 transition-all duration-100 my-[60px] px-[18%] sm:px-[20%] flex flex-col items-center justify-center'>
+              {/* A block to display the main logo */}
               <div className='block justify-center'>
                 <Logo size={25} />
               </div>
-              <p className='my-4'>Login to your account</p>
+              {/* A heading to indicate the purpose of the form */}
+              <p className='my-4'>Create a new account</p>
+              {/* A container for the Google sign-in button and custom inputs */}
               <div className='w-full'>
+                {/* A button for Google sign-in */}
                 <div className='h-[35px]'>
                   <Button color='white' isFullWidth={true}>
-                    <a className='text-black flex gap-3 w-full items-center justify-center font-poppins font-semibold'>
+                    <span className='text-black flex gap-3 w-full items-center justify-center font-poppins font-semibold'>
                       <Google size={25} />
                       Continue with Google
-                    </a>
+                    </span>
                   </Button>
                 </div>
+                {/* Dividers with the text 'or' to separate Google sign-in from custom inputs */}
                 <div className='relative flex py-[12px] items-center'>
                   <div className='ml-8 lg:ml-0 flex-grow border-t border-white-600'></div>
                   <span className='flex-shrink mx-4 text-white-400'>or</span>
                   <div className='mr-8 lg:mr-0 flex-grow border-t border-white-600'></div>
                 </div>
               </div>
-              {inputsData.map((pair, index) => (
-                <div
-                  className='flex flex-col gap-2 w-full px-0 sm:px-[15%]'
-                  key={index}
-                >
-                  {pair.map((columnData, columnIndex) => (
-                    <InputColumn
-                      key={columnIndex}
-                      label={columnData.label}
-                      inputs={columnData.inputs}
-                    />
-                  ))}
+              {/* A container for the custom input fields */}
+              <div className='flex flex-col gap-2 w-full px-0 sm:px-[15%]'>
+                {/* Email input field */}
+                <div>
+                  <p>Email</p>
+                  <TextInput
+                    text={email}
+                    setText={setEmail}
+                    color='white'
+                    fullwidth={true}
+                    type='email'
+                    placeholder='Your email address'
+                  />
                 </div>
-              ))}
+                {/* Password input field */}
+                <div>
+                  <p>Password</p>
+                  <TextInput
+                    text={password}
+                    setText={setPassword}
+                    color='white'
+                    fullwidth={true}
+                    type='password'
+                    placeholder='Enter a password'
+                  />
+                </div>
+              </div>
+              {/* Sign up button */}
               <div className='my-5 w-[100px] h-[40px]'>
                 <Button color='gold' isFullWidth={true}>
                   Sign In
@@ -123,7 +126,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-      {/* You can map through the filteredData to display the results */}
     </main>
   );
 }
