e3c4a291a989611e372cebe005bdb02f494efdfe
Mesach Harmasendro <68049342+Otzzu@users.noreply.github.com>
2023-10-17 08:41:29 +0700

    Connect FE BE Auth (#40)
    * feat: connect register fe with be
    
    * feat: connect login fe with be, add toaster and session provider
    
    * feat: Add metadata for auth page and change website icon
    
    ---------
    
    Co-authored-by: Moh. Fairuz Alauddin Yahya <fairuzy210@gmail.com>

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,11 +1,9 @@
 import Footer from '@/components/footer';
 import { ModalContextProvider } from '@/components/Modal/ModalContext';
-import Toast from '@/components/Toast';
 
 const SiteLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <body>
-      <Toast />
       <ModalContextProvider>{children}</ModalContextProvider>
       <Footer />
     </body>
