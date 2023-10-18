07868f2ec19494424c901b35adde5436a58d6d25
yonatan-nyo <yonatan.njoto@gmail.com>
2023-10-18 15:52:04 +0700

    fix: cascades class that hides the component of timeline

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -4,16 +4,18 @@ import Timeline from '@/components/Timeline';
 
 export default function Home() {
   return (
-    <main className='flex h-0 min-h-screen w-0 min-w-[100vw] flex-col overflow-y-scroll overflow-x-clip font-museo-muderno bg-slate-800'>
-      <Timeline
-        items={[
-          { date: new Date(2023, 0, 12), text: 'Open Regist PTC' },
-          { date: new Date(2023, 10, 12), text: 'Open Regist PTC' },
-          { date: new Date(2023, 10, 12), text: 'Open Regist PTC' },
-          { date: new Date(2023, 10, 12), text: 'Open Regist PTC' },
-          { date: new Date(2023, 10, 12), text: 'Open Regist PTC' },
-        ]}
-      />
+    <main className='flex min-h-screen w-screen flex-col overflow-x-clip font-museo-muderno bg-slate-800'>
+      <div className='h-fit w-full'>
+        <Timeline
+          items={[
+            { date: new Date(2023, 0, 12), text: 'Open Regist PTC' },
+            { date: new Date(2023, 10, 12), text: 'Open Regist PTC' },
+            { date: new Date(2023, 10, 12), text: 'Open Regist PTC' },
+            { date: new Date(2023, 10, 12), text: 'Open Regist PTC' },
+            { date: new Date(2023, 10, 12), text: 'Open Regist PTC' },
+          ]}
+        />
+      </div>
     </main>
   );
 }
