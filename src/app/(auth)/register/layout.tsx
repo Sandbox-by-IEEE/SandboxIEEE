c0f8477f99536edcc36e796e512f30cbe90e6f75
Moh. Fairuz Alauddin Yahya <fairuzy210@gmail.com>
2023-10-19 08:05:58 +0700

    feat: Update metadaa description

--- src/app/(auth)/register/layout.tsx
+++ src/app/(auth)/register/layout.tsx
@@ -10,13 +10,15 @@ export default function AuthLayout({
 
 export const metadata: Metadata = {
   title: 'Register | Sandbox IEEE ITB',
-  description: '',
+  description:
+    'The Sandbox by IEEE is a series of events providing opportunities to all young-minds through 3 key milestones consisting of a Grand Seminar, 2 competitions namely ProtoTech Contest (a practical electrical engineering contest) and Technovate Paper (a research and technical documents) Competition, and Exhibition. This event invites experts from various fields of work as trainers, judges and webinar speakers. Finalists from both ProtoTech Contest and TechNovate Paper Competition will be given time to pitch and showcase their products in front of the judging panels on the Exhibition day. All the final winners from both competitions will be bestowed during this time. The objective of this event is to establish innovative and practical solutions for a developing country like Indonesia. Additionally, this event also aims to educate the local society by unveiling and enhancing tools that foster tranquility and ease.',
   generator: 'Next.js',
   applicationName: 'Sandbox IEEE ITB',
   colorScheme: 'dark',
   openGraph: {
     title: 'Sandbox IEEE ITB',
-    description: '',
+    description:
+      'The Sandbox by IEEE is a series of events providing opportunities to all young-minds through 3 key milestones consisting of a Grand Seminar, 2 competitions namely ProtoTech Contest (a practical electrical engineering contest) and Technovate Paper (a research and technical documents) Competition, and Exhibition. This event invites experts from various fields of work as trainers, judges and webinar speakers. Finalists from both ProtoTech Contest and TechNovate Paper Competition will be given time to pitch and showcase their products in front of the judging panels on the Exhibition day. All the final winners from both competitions will be bestowed during this time. The objective of this event is to establish innovative and practical solutions for a developing country like Indonesia. Additionally, this event also aims to educate the local society by unveiling and enhancing tools that foster tranquility and ease.',
     url: 'https://sandbox.ieeeitb.com/',
     siteName: 'Sandbox IEEE ITB',
     images: [
@@ -33,7 +35,8 @@ export const metadata: Metadata = {
   twitter: {
     card: 'summary_large_image',
     title: 'Sandbox IEEE ITB',
-    description: '',
+    description:
+      'The Sandbox by IEEE is a series of events providing opportunities to all young-minds through 3 key milestones consisting of a Grand Seminar, 2 competitions namely ProtoTech Contest (a practical electrical engineering contest) and Technovate Paper (a research and technical documents) Competition, and Exhibition. This event invites experts from various fields of work as trainers, judges and webinar speakers. Finalists from both ProtoTech Contest and TechNovate Paper Competition will be given time to pitch and showcase their products in front of the judging panels on the Exhibition day. All the final winners from both competitions will be bestowed during this time. The objective of this event is to establish innovative and practical solutions for a developing country like Indonesia. Additionally, this event also aims to educate the local society by unveiling and enhancing tools that foster tranquility and ease.',
     images: [
       {
         url: 'https://sandbox.ieeeitb.com/link-preview.png',
