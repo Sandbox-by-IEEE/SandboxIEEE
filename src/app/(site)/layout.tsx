dc364c4adcf2ad772e5cb07034a707d26d851fcc
Moh. Fairuz Alauddin Yahya <fairuzy210@gmail.com>
2023-10-26 00:53:22 +0700

    test: Test remove footer to looking for analytics

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,13 +1,11 @@
 import { Metadata } from 'next';
 
-import Footer from '@/components/footer';
-
 const SiteLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='flex flex-col min-h-screen overflow-x-clip'>
       {/* <NavBar /> */}
       {children}
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
