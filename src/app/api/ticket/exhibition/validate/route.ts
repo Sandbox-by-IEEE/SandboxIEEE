ea8991ef27f45a9fdd05c5c3ec93a2af36806dd3
Mesach Harmasendro <68049342+Otzzu@users.noreply.github.com>
2023-10-27 09:49:19 +0700

    Ticketing (#14)
    * chore: Init ticketing branch
    
    * feat: Add api to purchase ticket
    
    * feat: Add api for ticket purchases have been verified
    
    * fix: fix confict
    
    * feat: Add api for validate ticket
    
    * refactor: Change code for sending email and change smtp settings
    
    * fix: try to fix bugs email doesn't send
    
    * refactor: change .env file
    
    * refactor: Change handle error in ticket api and validate api
    
    * chore: prepare react-email
    
    * feat: add ticket exhibition status in session
    
    * feat: change body request for ticket compe register and change how to send email
    
    * feat: add api for register 2, add api for send email, edit some code in api voting and ticket
    
    * fix: fix error in auth options
    
    ---------
    
    Co-authored-by: Moh. Fairuz Alauddin Yahya <fairuzy210@gmail.com>

---
+++ src/app/api/ticket/exhibition/validate/route.ts
@@ -0,0 +1,72 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import { prisma } from '@/lib/db';
+
+export async function PATCH(req: NextRequest) {
+  try {
+    const { ticketId, userId } = await req.json();
+
+    if (!ticketId || !userId) {
+      return NextResponse.json(
+        { message: 'Missing ticket id or user id' },
+        { status: 400 },
+      );
+    }
+
+    const existingTicket = await prisma.ticketExhibition.findUnique({
+      where: {
+        id: ticketId,
+      },
+    });
+
+    if (!existingTicket) {
+      return NextResponse.json(
+        { message: 'Ticket id is invalid!!!' },
+        { status: 404 },
+      );
+    }
+
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!existingUser) {
+      return NextResponse.json(
+        { message: 'User id is invalid!!!' },
+        { status: 404 },
+      );
+    }
+
+    if (existingTicket.active) {
+      return NextResponse.json(
+        { message: 'Ticket has been used!!' },
+        { status: 400 },
+      );
+    }
+
+    const updatedTicket = await prisma.ticketExhibition.update({
+      where: {
+        id: ticketId,
+      },
+      data: {
+        active: true,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        ticket: updatedTicket,
+        user: existingUser,
+        message: 'Validate ticket succesful',
+      },
+      { status: 200 },
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log('VALIDATE_TICKET: ', error);
+      return NextResponse.json({ message: error.message }, { status: 500 });
+    }
+  }
+}
