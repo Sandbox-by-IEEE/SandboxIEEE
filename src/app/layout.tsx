4a3ee9f5728ac7f30b49ab8d2915efaf196c6599
Moh. Fairuz Alauddin Yahya <fairuzy210@gmail.com>
2023-10-27 22:36:07 +0700

    fix: Update hotjar id and uninstall partydown

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,12 +57,12 @@ export default function RootLayout({
         strategy='worker'
         dangerouslySetInnerHTML={{
           __html: `(function(h,o,t,j,a,r){
-          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-          h._hjSettings={hjid:3712665,hjsv:6};
-          a=o.getElementsByTagName('head')[0];
-          r=o.createElement('script');r.async=1;
-          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-          a.appendChild(r);
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3712758,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
         })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
         }}
       />
