56d535334c014af3d16c99be5620cbbb8011f136
Otzzu <mesachharmasendro@gmail.com>
2023-10-29 21:50:42 +0700

    refactor: change code in api sendemail

--- src/app/(site)/events/tpc/registration/page.tsx
+++ src/app/(site)/events/tpc/registration/page.tsx
@@ -287,7 +287,7 @@ export default function TPCRegist() {
           status: 'success',
           description: bodyResponse.message,
         });
-        router.push('/events/tpc/registration');
+        router.push('/events/tpc');
         localStorage.removeItem(inputDataHistoryKey);
       }
     } catch (err) {
