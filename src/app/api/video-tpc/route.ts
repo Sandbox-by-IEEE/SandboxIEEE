44c5c49201a122e784787b1d211f1c18cd3c2731
Otzzu <mesachharmasendro@gmail.com>
2024-01-24 19:43:23 +0700

    feat: add deadline check in video tpc api

--- src/app/api/video-tpc/route.ts
+++ src/app/api/video-tpc/route.ts
@@ -12,7 +12,9 @@ export async function POST(req: NextRequest) {
     const session = await getServerSession(authOptions);
 
     if (!session) {
-      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 401  });
+
+      
     }
 
     const body = await req.json();
