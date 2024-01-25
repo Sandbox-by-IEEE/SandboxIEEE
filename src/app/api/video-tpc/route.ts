a8731254551a3feff57a506e5d1edd4a12eeb5b6
Otzzu <mesachharmasendro@gmail.com>
2024-01-25 15:05:06 +0700

    feat: add send email in video tpc api

--- src/app/api/video-tpc/route.ts
+++ src/app/api/video-tpc/route.ts
@@ -1,5 +1,8 @@
+import Email from '@/components/emails/Emails';
 import { authOptions } from '@/lib/authOptions';
 import { prisma } from '@/lib/db';
+import { transporter } from '@/lib/mailTransporter';
+import { render } from '@react-email/render';
 import moment from 'moment-timezone';
 import { getServerSession } from 'next-auth';
 import { NextRequest, NextResponse } from 'next/server';
@@ -12,9 +15,7 @@ export async function POST(req: NextRequest) {
     const session = await getServerSession(authOptions);
 
     if (!session) {
-      return NextResponse.json({ message: 'Unauthorized' }, { status: 401  });
-
-
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
     }
 
     const body = await req.json();
@@ -71,14 +72,24 @@ export async function POST(req: NextRequest) {
       );
     }
 
-    if (existingTeam.regist3Data?.statusPayment !== 'verified') {
+    if (!existingTeam.regist3Data) {
+      return NextResponse.json(
+        { message: 'You did not register at stage 3' },
+        { status: 400 },
+      );
+    }
+
+    if (existingTeam.regist3Data.statusPayment !== 'verified') {
       return NextResponse.json(
         { message: 'Your payment for stage 3 has not been verified' },
         { status: 400 },
       );
     }
 
-    if (moment().tz("Asia/Jakarta").unix() > moment.tz("2024-02-15 18:00", "Asia/Jakarta").unix()) {
+    if (
+      moment().tz('Asia/Jakarta').unix() >
+      moment.tz('2024-02-15 18:00', 'Asia/Jakarta').unix()
+    ) {
       return NextResponse.json(
         {
           message: 'You past the deadline',
@@ -110,6 +121,63 @@ export async function POST(req: NextRequest) {
       },
     });
 
+    const dataSheet = {
+      registId: existingTeam.regist3Data.id,
+      linkVideo: linkVideo,
+    };
+
+    const response = await fetch(
+      `${process.env.API_SHEET_TPC_URL}?type=video` || '',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dataSheet),
+      },
+    );
+
+    const resBody = await response.json();
+
+    // console.log(resBody)
+
+    if (resBody.status > 299 || resBody.status < 200) {
+      throw new Error(`Failed to create regist3 data, ${resBody.message}`);
+    }
+
+    const content = `
+    Thank you for your participation in the Sandbox project. Your data has been received, please wait for confirmation from the Sandbox team.
+
+    This is your link video: ${linkVideo}
+    `;
+
+    const promises: any[] = [];
+
+    for (let i = 0; i < existingTeam.members.length; i++) {
+      const mailOptions = {
+        from: '"The Sandbox by IEEE" <sandboxieeewebsite@gmail.com>',
+        to: existingTeam.members[i].email,
+        subject: `[SANDBOX]
+            Video Campaign Submission'
+        }`,
+        html: render(
+          Email({
+            content,
+            heading: `Video Campaign Submission`,
+            name: existingTeam.members[i].name || '',
+          }),
+          { pretty: true },
+        ),
+      };
+
+      promises.push(transporter.sendMail(mailOptions));
+    }
+
+    await Promise.all(promises);
+
+    // eslint-disable-next-line no-console
+    console.log('POST_VIDEO_TPC_3: email was sent');
+
     return NextResponse.json(
       {
         data: { ...updatedKarya, countVote: updatedKarya.countVote.toString() },
@@ -119,7 +187,7 @@ export async function POST(req: NextRequest) {
     );
   } catch (error) {
     if (error instanceof Error) {
-      console.log('UPLOAD_VIDEO_TPC: ', error);
+      console.log('POST_VIDEO_TPC_3: ', error);
       return NextResponse.json({ message: error.message }, { status: 500 });
     }
   }
