481f77e92575f9888e92abd5324ba06dbe2277e2
Otzzu <mesachharmasendro@gmail.com>
2024-02-12 21:44:42 +0700

    feat: counter exhi participants api

---
+++ src/app/api/ticket/exhibition/countparticipants/route.ts
@@ -0,0 +1,45 @@
+import { prisma } from '@/lib/db';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  try {
+    const count = await prisma.ticketGS.findMany({
+      where: {
+        transactionDetail: {
+          OR: [
+            {
+              status: 'pending',
+            },
+            {
+              status: 'success',
+            },
+          ],
+        },
+      },
+      select: {
+        id: true
+      }
+    });
+
+    return NextResponse.json(
+      {
+        message: 'get data succesfull',
+        data: { count: count.length },
+      },
+      {
+        status: 200,
+      },
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        {
+          message: 'Something went wrong',
+        },
+        {
+          status: 500,
+        },
+      );
+    }
+  }
+}
