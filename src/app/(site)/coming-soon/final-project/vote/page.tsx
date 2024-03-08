391b7fff708e14517115f912597b740d119ae11d
Anas Fathurrahman <anasfathurrahman.edu@gmail.com>
2024-03-08 08:24:02 +0700

    feat: add team details

--- src/app/(site)/coming-soon/final-project/vote/page.tsx
+++ src/app/(site)/coming-soon/final-project/vote/page.tsx
@@ -19,35 +19,183 @@ const CoverFlowSlider = dynamic(
 );
 
 export default function FinalProjectVote() {
-  const items = [
+  // Fetch Data DatoCMS
+  // useEffect(() => {
+  //   const CMS_QUERY = `
+  //   {
+  //     exhibition {
+  //       backgroundImage {
+  //         url
+  //         title
+  //         width
+  //         height
+  //       }
+  //       buttonShowFinal
+  //       buttonTextRegister
+  //       buttonTextSeeMore
+  //       buttonTextSeeMote
+  //       countdownTitle
+  //       embedLocationUrl
+  //       urlLocationMaps
+  //       buttonTextBackgroundOne
+  //       explanationDescription {
+  //         value
+  //       }
+  //       explanationTitle
+  //       faqSectionTitle
+  //       finalProjectTitle
+  //       guideDescription {
+  //         value
+  //       }
+  //       guideTitle
+  //       imageMascot {
+  //         height
+  //         url
+  //         title
+  //         width
+  //       }
+  //       targetDate
+  //       timelineSectionTitle
+  //       titleExhibitionPage
+  //       titleLocation
+  //       ptcSubtitle
+  //       tpcSubtitle
+  //     }
+  //     allFinalProjectsPtcExhibitions(orderBy: teamsName_ASC) {
+  //       topic
+  //       teamsName
+  //       projectsUrl
+  //       image {
+  //         url
+  //         width
+  //         height
+  //         title
+  //       }
+  //       id
+  //     }
+  //     allFinalProjectsTpcExhibitions(orderBy: teamsName_ASC) {
+  //       image {
+  //         url
+  //         title
+  //         width
+  //         height
+  //       }
+  //       projectsUrl
+  //       teamsName
+  //       topic
+  //       id
+  //     }
+  //     allFaqExhibitions(orderBy: question_ASC) {
+  //       id
+  //       answer {
+  //         value
+  //       }
+  //       question
+  //     }
+  //     allTimelineExhibitions(orderBy: date_ASC) {
+  //       text
+  //       date
+  //     }
+  //   }`;
+
+  //   const getData = async () => {
+  //     const {
+  //       exhibition,
+  //       allFinalProjectsTpcExhibitions: TPCData,
+  //       allFinalProjectsPtcExhibitions: PTCData,
+  //       allFaqExhibitions: faqData,
+  //       allTimelineExhibitions: timelineData,
+  //     }: ExhibitionDataProps = await performRequest({
+  //       query: CMS_QUERY,
+  //       revalidate: 0,
+  //     });
+  //   }
+
+  //   getData();
+
+  // });
+
+  const PTC = [
     {
       id: 1,
       label: 'Item 1',
-      name: "Team's Name",
-      imgUrl:
-        'https://res.cloudinary.com/dbdrtwtzl/image/upload/v1708269822/koxqcs2thoz97d4mqt8c.png',
+      name: 'Eureka',
+      imgUrl: 'https://sandbox.ieeeitb.com/sandbox-gold.svg',
       instagramLink: 'https://instagram.com',
       introductionLink: 'https://instagram.com',
-      topic: 'Topic Name Topic Name Topic Name',
+      topic: 'Green Keys',
+      desc: ' Keyboard ramah lingkungan kami mengurangi limbah elektronik dengan casing dari filamen PLA.',
+    },
+    {
+      id: 2,
+      label: 'Item 2',
+      name: 'FansPakBandung',
+      imgUrl: 'https://sandbox.ieeeitb.com/sandbox-gold.svg',
+      instagramLink: 'https://instagram.com',
+      introductionLink: 'https://instagram.com',
+      topic: 'AFMS',
+      desc: ' Our system revolutionizes agriculture with smart sensors and machine learning for efficient, sustainable farming.',
+    },
+    {
+      id: 3,
+      label: 'Item 3',
+      name: 'HehoGawk',
+      imgUrl: 'https://sandbox.ieeeitb.com/sandbox-gold.svg',
+      instagramLink: 'https://instagram.com',
+      introductionLink: 'https://instagram.com',
+      topic: 'ReformX',
+      desc: ' Reform The Reformer',
+    },
+    {
+      id: 3,
+      label: 'Item 4',
+      name: 'Bangan',
+      imgUrl: 'https://sandbox.ieeeitb.com/sandbox-gold.svg',
+      instagramLink: 'https://instagram.com',
+      introductionLink: 'https://instagram.com',
+      topic: 'Hysteresis Dynamic Response Adapter (HYDRA)',
+      desc: ' HYDRA offers a bidirectional inverter with an extremely fast.',
+    },
+    {
+      id: 4,
+      label: 'Item 5',
+      name: 'WANGSIT NEW GEN',
+      imgUrl: 'https://sandbox.ieeeitb.com/sandbox-gold.svg',
+      instagramLink: 'https://instagram.com',
+      introductionLink: 'https://instagram.com',
+      topic: 'Ufobi',
+      desc: " We don't have to sacrifice a strong economy for a healthy environment. Ufobi is the solution.",
+    },
+  ];
+
+  const TPC = [
+    {
+      id: 1,
+      label: 'Item 1',
+      name: 'Doa (W)ibu',
+      imgUrl: 'https://sandbox.ieeeitb.com/sandbox-gold.svg',
+      instagramLink: 'https://instagram.com',
+      introductionLink: 'https://instagram.com',
+      topic:
+        'Microbial Fuel Cell sebagai Solusi Green Technology yang Inovatif untuk Pengelolaan Limbah Rumah Tangga dan Pembangkitan Energi Listrik Skala Mikro',
       desc: 'Take Action',
     },
     {
       id: 2,
       label: 'Item 2',
-      name: "Team's Name",
-      imgUrl:
-        'https://res.cloudinary.com/dbdrtwtzl/image/upload/v1708269822/koxqcs2thoz97d4mqt8c.png',
+      name: 'BIO-WANA',
+      imgUrl: 'https://sandbox.ieeeitb.com/sandbox-gold.svg',
       instagramLink: 'https://instagram.com',
       introductionLink: 'https://instagram.com',
-      topic: 'Topic Name Topic Name Topic Name',
-      desc: 'Take Action',
+      topic:
+        'PERANCANGAN PERSEMAIAN BALANGERAN (Shorea Balangeran): INFRASTRUKTUR BERKELANJUTAN DALAM MENDUKUNG UPAYA RESTORASI LAHAN GAMBUT DI IBU KOTA NUSANTARA',
+      desc: '',
     },
     {
       id: 3,
       label: 'Item 3',
       name: "Team's Name",
-      imgUrl:
-        'https://res.cloudinary.com/dbdrtwtzl/image/upload/v1708269822/koxqcs2thoz97d4mqt8c.png',
+      imgUrl: 'https://sandbox.ieeeitb.com/sandbox-gold.svg',
       instagramLink: 'https://instagram.com',
       introductionLink: 'https://instagram.com',
       topic: 'Topic Name Topic Name Topic Name',
@@ -57,8 +205,7 @@ export default function FinalProjectVote() {
       id: 3,
       label: 'Item 4',
       name: "Team's Name",
-      imgUrl:
-        'https://res.cloudinary.com/dbdrtwtzl/image/upload/v1708269822/koxqcs2thoz97d4mqt8c.png',
+      imgUrl: 'https://sandbox.ieeeitb.com/sandbox-gold.svg',
       instagramLink: 'https://instagram.com',
       introductionLink: 'https://instagram.com',
       topic: 'Topic Name Topic Name Topic Name',
@@ -68,8 +215,7 @@ export default function FinalProjectVote() {
       id: 4,
       label: 'Item 5',
       name: "Team's Name",
-      imgUrl:
-        'https://res.cloudinary.com/dbdrtwtzl/image/upload/v1708269822/koxqcs2thoz97d4mqt8c.png',
+      imgUrl: 'https://sandbox.ieeeitb.com/sandbox-gold.svg',
       instagramLink: 'https://instagram.com',
       introductionLink: 'https://instagram.com',
       topic: 'Topic Name Topic Name Topic Name',
@@ -90,17 +236,21 @@ export default function FinalProjectVote() {
       <div className='scale-125'>
         <TitleSection>Vote For Final Project&apos; s Team</TitleSection>
       </div>
-      <p className='text-white font-poppins max-w-[70em] py-16 tracking-widest font-extralight'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a ipsum
-        at sapien dignissim interdum sit amet sit amet nisl. Vestibulum quis
-        volutpat ipsum, consequat blandit mi. Vivamus id porta dolor, vel
-        tristique libero. Cras scelerisque purus in quam posuere blandit. Lorem
-        ipsum dolor sit amet, consectetur adipiscing
+      <p className='text-white font-poppins max-w-[70em] py-16 tracking-widest font-extralight text-center'>
+        Welcome to the voting page for The Sandbox Technovate Paper Competition
+        and Prototech Contest. As we explore groundbreaking ideas and
+        technological advancements, your input is invaluable. By participating
+        in this vote, you contribute to the evolution of innovation. Take a
+        thoughtful look at the submissions and cast your vote for the most
+        promising vision. Together, let’s pave the way for a brighter tomorrow!
       </p>
 
+      <div className='scale-100'>
+        <TitleSection>Technovate Paper Competition</TitleSection>
+      </div>
       <section className='hidden w-full max-w-[1200px] h-fit md:flex flex-row py-8 items-center justify-evenly overflow-visible'>
         <CoverFlowSlider>
-          {items.map((item) => (
+          {TPC.map((item) => (
             <div
               key={item.id}
               className={`carousel-item bg-gradient-to-b from-[#FFE1B9] to-[#AB814EDB] rounded-lg flex flex-col justify-center items-center  py-8 gap-2 cursor-pointer transform transition-transform h-[80%] max-h-[80%] text-lg px-4 text-center`}
@@ -117,13 +267,13 @@ export default function FinalProjectVote() {
                 />
               </div>
               <div className='flex justify-center w-fit gap-4 text-blue-800'>
-                <a
+                {/* <a
                   href={item.instagramLink}
                   className='flex gap-2 items-center'
                 >
                   <AiOutlineLink />
                   instagram
-                </a>
+                </a> */}
                 <a
                   href={item.introductionLink}
                   className='flex gap-2 items-center'
@@ -147,10 +297,10 @@ export default function FinalProjectVote() {
 
       <section className='md:hidden w-full max-w-[1200px] h-fit flex flex-row sm:py-8 items-center justify-evenly overflow-visible'>
         <NormalSlider>
-          {items.map((item) => (
+          {TPC.map((item) => (
             <div
               key={item.id}
-              className={`carousel-item bg-gradient-to-b from-[#FFE1B9] to-[#AB814EDB] rounded-lg flex flex-col justify-center items-center  py-8 gap-2 cursor-pointer transform transition-transform h-[80%] max-h-[80%] text-lg px-4 text-center`}
+              className={`carousel-item m-auto w-full bg-gradient-to-b from-[#FFE1B9] to-[#AB814EDB] rounded-lg flex flex-col justify-center items-center  py-8 gap-2 cursor-pointer transform transition-transform h-[80%] max-h-[80%] text-lg px-4 text-center`}
               style={{ maxWidth: 'fit-content', width: 'fit-content' }}
             >
               <p className='text-2xl font-bold'>{item.name}</p>
@@ -164,13 +314,111 @@ export default function FinalProjectVote() {
                 />
               </div>
               <div className='flex justify-center w-fit gap-4 text-blue-800'>
-                <a
+                {/* <a
                   href={item.instagramLink}
                   className='flex gap-2 items-center'
                 >
                   <AiOutlineLink />
                   instagram
+                </a> */}
+                <a
+                  href={item.introductionLink}
+                  className='flex gap-2 items-center'
+                >
+                  <AiOutlineLink />
+                  Introduction
                 </a>
+              </div>
+
+              <p className='font-bold text-2xl pb-4'>
+                &apos;{item.topic}:{item.desc}&apos;
+              </p>
+
+              <Button type='button' color='green' onClick={handleButtonVote}>
+                Vote
+              </Button>
+            </div>
+          ))}
+        </NormalSlider>
+      </section>
+
+      {/* Bagian 2 */}
+      <div className='scale-100 mt-20'>
+        <TitleSection>Prototech Contest</TitleSection>
+      </div>
+      <section className='hidden w-full max-w-[1200px] h-fit md:flex flex-row py-8 items-center justify-evenly overflow-visible'>
+        <CoverFlowSlider>
+          {PTC.map((item) => (
+            <div
+              key={item.id}
+              className={`carousel-item bg-gradient-to-b from-[#FFE1B9] to-[#AB814EDB] rounded-lg flex flex-col justify-center items-center  py-8 gap-2 cursor-pointer transform transition-transform h-[80%] max-h-[80%] text-lg px-4 text-center`}
+              style={{ maxWidth: 'fit-content', width: 'fit-content' }}
+            >
+              <p className='text-2xl font-bold'>{item.name}</p>
+              <div className='rounded-full w-[30%] overflow-hidden'>
+                <Image
+                  src={item.imgUrl}
+                  alt={item.name}
+                  className='object-cover w-full h-auto aspect-square object-center'
+                  width={600}
+                  height={600}
+                />
+              </div>
+              <div className='flex justify-center w-fit gap-4 text-blue-800'>
+                {/* <a
+                  href={item.instagramLink}
+                  className='flex gap-2 items-center'
+                >
+                  <AiOutlineLink />
+                  instagram
+                </a> */}
+                <a
+                  href={item.introductionLink}
+                  className='flex gap-2 items-center'
+                >
+                  <AiOutlineLink />
+                  Introduction
+                </a>
+              </div>
+
+              <p className='font-bold text-2xl pb-4'>
+                &apos;{item.topic}:{item.desc}&apos;
+              </p>
+
+              <Button type='button' color='green'>
+                Vote
+              </Button>
+            </div>
+          ))}
+        </CoverFlowSlider>
+      </section>
+
+      <section className='md:hidden w-full max-w-[1200px] h-fit flex flex-row sm:py-8 items-center justify-evenly overflow-visible'>
+        <NormalSlider>
+          {PTC.map((item) => (
+            <div
+              key={item.id}
+              className={`carousel-item bg-gradient-to-b from-[#FFE1B9] to-[#AB814EDB] rounded-lg flex flex-col justify-center items-center  py-8 gap-2 cursor-pointer transform transition-transform h-[80%] max-h-[80%] text-lg px-4 text-center w-full m-auto`}
+              style={{ maxWidth: 'fit-content', width: 'fit-content' }}
+            >
+              <p className='text-2xl font-bold'>{item.name}</p>
+              <div className='rounded-full w-[200px] overflow-hidden'>
+                <Image
+                  src={item.imgUrl}
+                  alt={item.name}
+                  className='object-cover w-full h-auto aspect-square object-center'
+                  width={600}
+                  height={600}
+                />
+              </div>
+              <div className='flex justify-center w-fit gap-4 text-blue-800'>
+                {/* <a
+                  href={item.instagramLink}
+                  className='flex gap-2 items-center'
+                >
+                  <AiOutlineLink />
+                  instagram
+                </a> */}
                 <a
                   href={item.introductionLink}
                   className='flex gap-2 items-center'
