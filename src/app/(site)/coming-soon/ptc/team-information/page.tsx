edc64ebc849da08c04c0ec80f449edcab73eeb87
yonatan-nyo <yonatan.njoto@gmail.com>
2024-03-08 22:35:24 +0700

    style: fixed responsiveness on team profile

--- src/app/(site)/coming-soon/ptc/team-information/page.tsx
+++ src/app/(site)/coming-soon/ptc/team-information/page.tsx
@@ -138,7 +138,7 @@ const PTCTeamInformation = () => {
           Team Profile
         </p>
         <div className='flex flex-wrap gap-4 py-6'>
-          <div className='aspect-square w-0 min-w-[27rem] flex-grow grid grid-cols-2 h-fit gap-8 rounded-lg'>
+          <div className='aspect-square w-0 min-w-full sm:min-w-[27rem] flex-grow grid grid-cols-2 h-fit gap-8 rounded-lg'>
             <div className='bg-slate-100 rounded-full flex justify-center items-center'>
               <Image
                 src='/Group_1289.png'
@@ -176,7 +176,7 @@ const PTCTeamInformation = () => {
               />
             </div>
           </div>
-          <section className='flex-grow w-0 min-w-[5rem] bg-[#49784F] text-white font-bold p-4 rounded-lg'>
+          <section className='flex-grow w-0 min-w-[10rem] flex-shrink-0 bg-[#49784F] text-white font-bold p-4 rounded-lg'>
             <LableValue lable='Team Name'>{teamInfo.data.teamName}</LableValue>
             <LableValue lable='Team Leader'>
               {teamInfo.data.chairmanName}
