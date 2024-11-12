fd9524a6ad641a3407481c67fb9e1011d9abbbf9
Lint Action <lint-action@samuelmeuli.com>
2024-11-12 12:47:03 +0000

    Fix code style issues with Prettier

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,38 +8,41 @@ import NavBar from '@/components/Navbar';
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className='flex flex-col min-h-screen'>
       <NavBar />
       <main className="className='relative w-full z-5 flex flex-col min-h-screen justify-center items-center py-28 pt-14 lg:py-28 lg:pt-20 gap-10 lg:gap-16 px-8 sm:px-14 md:px-24 lg:px-44 relative bg-[#040B15] font-poppins flex-grow text-white">
-        <div className="text-center">
-          <h1 className="font-bold md:text-[96px] leading-normal text-[48px]">404</h1>
-          <h2 className="md:font-bold md:text-[32px] leading-[48px] text-[16px] font-normal">
+        <div className='text-center'>
+          <h1 className='font-bold md:text-[96px] leading-normal text-[48px]'>
+            404
+          </h1>
+          <h2 className='md:font-bold md:text-[32px] leading-[48px] text-[16px] font-normal'>
             Fixing there and there
           </h2>
         </div>
         {/*Shadow*/}
         <div
-          className="absolute md:w-[50%] md:h-[50%] md:rounded-[50%] w-[15%] h-[15%] opacity-50"
+          className='absolute md:w-[50%] md:h-[50%] md:rounded-[50%] w-[15%] h-[15%] opacity-50'
           style={{
-            background: 'radial-gradient(50% 50% at 50% 50%, rgba(159, 42, 57, 0.45) 0%, rgba(11, 53, 15, 0) 100%)',
+            background:
+              'radial-gradient(50% 50% at 50% 50%, rgba(159, 42, 57, 0.45) 0%, rgba(11, 53, 15, 0) 100%)',
             mixBlendMode: 'screen',
             transform: 'rotate(135deg)',
           }}
         />
         {/* Images */}
         <Image
-          src="/404assets/left-image-error.png"
-          alt="Left Decorative Image"
-          className="absolute md:top-[10%] md:left-[15.5%] md:w-[35%] w-[60%] top-[12%] left-[-5%]"
+          src='/404assets/left-image-error.png'
+          alt='Left Decorative Image'
+          className='absolute md:top-[10%] md:left-[15.5%] md:w-[35%] w-[60%] top-[12%] left-[-5%]'
           width={0}
           height={0}
           sizes='100vw'
           priority
         />
         <Image
-          src="/404assets/right-image-error.png"
-          alt="Right Decorative Image"
-          className="absolute md:top-[10%] md:right-[8%] md:w-[39%] w-[58%] top-[25%] right-[-10%]"
+          src='/404assets/right-image-error.png'
+          alt='Right Decorative Image'
+          className='absolute md:top-[10%] md:right-[8%] md:w-[39%] w-[58%] top-[25%] right-[-10%]'
           width={0}
           height={0}
           sizes='100vw'
