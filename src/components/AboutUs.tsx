056fe23644e99e69836349e97c6f030ae9876c0b
Muhammad Fithra Rizki <muhammadfithrarizki@Muhammads-MacBook-Pro.local>
2024-11-19 14:16:03 +0700

    fix: responsive bug

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -3,32 +3,32 @@ import GradientBox from "./GradientBox";
 
 const AboutUs = () => {
     return (
-        <div className="mb-auto relative justify-center content-center item-center bg-[#071D10]">
+        <div className="mb-auto relative justify-center content-center bg-[#071D10]">
             <div
-                className="font-poppins text-5xl flex justify-center font-bold space-x-4 px-5 py-12 gap-x-2 text-white"
+                className="font-poppins text-[24px] lg:text-5xl flex justify-center font-bold py-6 lg:py-12 text-white"
                 data-aos="fade-up"
                 data-aos-duration="1300"
             >
                 <h1>What is Sandbox?</h1>
             </div>
 
-            <GradientBox type="blue" className="lg:h-[363px] lg:w-[1191px]">
+            <GradientBox type="blue" className="h-[227px] w-[336px] lg:h-[363px] lg:w-[1191px]">
                 <div className="flex flex-col items-start text-white content-center text-center items-center">
                     {/* Title */}
                     <div>
                         <h1
-                            className="font-poppins text-3xl font-bold leading-tight tracking-wide"
+                            className="font-poppins text-[20px] lg:text-3xl  font-bold leading-tight"
                             data-aos="fade-up"
                             data-aos-duration="1500"
                         >
-                            Lorem ipsum dolor sit amet
+                            Identity
                         </h1>
                     </div>
 
                     {/* Paragraph */}
                     <div>
                         <p
-                            className="font-poppins text-lg leading-relaxed tracking-wide max-w-2xl mt-8"
+                            className="font-poppins text-[12px] lg:text-lg leading-relaxed tracking-wide max-w-[17rem] lg:max-w-2xl mt-4 lg:mt-8 lg:mb-12"
                             data-aos="fade-up"
                             data-aos-duration="1500"
                         >
@@ -42,36 +42,36 @@ const AboutUs = () => {
             </GradientBox>
 
             <div
-                className="p-10 flex flex-row font-poppins text-center justify-center space-x-24"
+                className="p-8 flex flex-col lg:flex-row font-poppins text-center justify-center gap-6 lg:space-x-24"
                 data-aos="zoom-in"
                 data-aos-duration="1100"
             >
-                <div className="flex flex-col items-center space-y-1 opacity-60 hover:opacity-100 animation-all duration-500 delay-300">
-                    <div className="text-5xl py-1 font-bold bg-gradient-to-r from-[#8C69F9] to-[#533E93] text-transparent bg-clip-text"
-                    style = {{
-                        textShadow: '0 0 20px rgba(255, 255, 255, 0.2)'
-                    }}>
+                <div className="flex flex-col items-center  opacity-100 lg:opacity-60 hover:opacity-100 animation-all duration-500 delay-300">
+                    <div className="text-[24px] lg:text-[48px] py-1 font-bold bg-gradient-to-r from-[#8C69F9] to-[#533E93] text-transparent bg-clip-text"
+                        style={{
+                            textShadow: '0 0 20px rgba(255, 255, 255, 0.2)'
+                        }}>
                         +49,000
                     </div>
-                    <p className="text-white max-w-[12rem]">Lorem ipsum dolor sit amet consectetur.</p>
+                    <p className="text-[12px] lg:text-[24px] text-white max-w-[16rem]">Lorem ipsum dolor sit amet consectetur.</p>
                 </div>
-                <div className="flex flex-col items-center space-y-1">
-                    <div className="text-6xl py-1 font-bold bg-gradient-to-r from-[#8C69F9] to-[#533E93] text-transparent bg-clip-text"
-                    style = {{
-                        textShadow: '0 0 20px rgba(255, 255, 255, 0.2)'
-                    }}>
+                <div className="flex flex-col items-center ">
+                    <div className="text-[24px] lg:text-[64px] py-1 font-bold bg-gradient-to-r from-[#8C69F9] to-[#533E93] text-transparent bg-clip-text"
+                        style={{
+                            textShadow: '0 0 20px rgba(255, 255, 255, 0.2)'
+                        }}>
                         +49,000
                     </div>
-                    <p className="text-xl text-white max-w-[16rem]">Lorem ipsum dolor sit amet consectetur.</p>
+                    <p className="text-[12px] lg:text-[24px] text-white max-w-[16rem]">Lorem ipsum dolor sit amet consectetur.</p>
                 </div>
-                <div className="flex flex-col items-center space-y-1 opacity-60 hover:opacity-100 animation-all duration-500 delay-300">
-                    <div className="text-5xl py-1 font-bold bg-gradient-to-r from-[#8C69F9] to-[#533E93] text-transparent bg-clip-text"
-                    style = {{
-                        textShadow: '0 0 20px rgba(255, 255, 255, 0.2)'
-                    }}>
+                <div className="flex flex-col items-center opacity-100 lg:opacity-60 hover:opacity-100 animation-all duration-500 delay-300">
+                    <div className="text-[24px] lg:text-[48px] py-1 font-bold bg-gradient-to-r from-[#8C69F9] to-[#533E93] text-transparent bg-clip-text"
+                        style={{
+                            textShadow: '0 0 20px rgba(255, 255, 255, 0.2)'
+                        }}>
                         +49,000
                     </div>
-                    <p className="text-white max-w-[12rem]">Lorem ipsum dolor sit amet consectetur.</p>
+                    <p className="text-[12px] lg:text-[24px] text-white max-w-[16rem]">Lorem ipsum dolor sit amet consectetur.</p>
                 </div>
             </div>
         </div>
