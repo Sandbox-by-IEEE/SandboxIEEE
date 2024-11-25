ffdcaae242b663bd45771160c62f0b5aaf2a9842
varelll <19623183@std.stei.itb.ac.id>
2024-11-25 10:33:09 +0700

    feat: add PTC page

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -10,7 +10,6 @@ import { StructuredText } from 'react-datocms/structured-text';
 
 import ArrowDropdownIcon from '@/components/icons/ArrowDropdownIcon';
 
-
 export const FAQ = ({
   question,
   answer,
@@ -23,7 +22,7 @@ export const FAQ = ({
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div
-      className='bg-dark-green p-5 lg:p-6 w-full flex flex-col cursor-pointer'
+      className='bg-gradient-to-r from-[#28575c] to-[#0d2d32] p-2 lg:p-6 w-full flex flex-col cursor-pointer'
       onClick={() => setIsOpen(!isOpen)}
       data-aos={aos || 'flip-down'}
     >
@@ -32,24 +31,35 @@ export const FAQ = ({
         {/* Question */}
         <h4
           data-aos='zoom-in'
-          className='font-poppins bg-gradient-to-tr from-[#af8954] via-[#cfb57c] to-[#ede1a2] text-left text-transparent bg-clip-text text-base sm:text-lg lg:text-xl font-semibold'
+          className='font-poppins bg-gradient-to-tr from-[#af8954] via-[#cfb57c] to-[#ede1a2] text-left text-white bg-clip-text text-sm sm:text-base lg:text-base font-semibold'
         >
           {question}
         </h4>
         {/* Arrow */}
         <ArrowDropdownIcon
           size={25}
-          className={`fill-cream-secondary-normal transition-all duration-300 w-[25px] aspect-square ${
+          className={`fill-white transition-all duration-300 w-5 mr-2 aspect-square ${
             isOpen ? 'rotate-0' : 'rotate-180'
           }`}
         />
       </div>
+
+      {/* Garis Pembatas */}
+      <div
+        className={`transition-all duration-300 ${
+          isOpen ? 'opacity-100 my-4' : 'opacity-0 h-0'
+        }`}
+      >
+        <hr className='border-t border-gray-400/50' />
+      </div>
+
+      {/* Answer */}
       <span
         className={`${
           isOpen
             ? 'opacity-100 mt-6 h-full pointer-events-none'
-            : ' opacity-0 m-0 h-0 pointer-events-none'
-        } transition-all duration-300 text-cream-secondary-light text-sm sm:text-base lg:text-lg font-poppins text-left`}
+            : 'opacity-0 m-0 h-0 pointer-events-none'
+        } transition-all duration-300 text-white text-sm sm:text-base lg:text-base font-poppins text-left`}
       >
         <StructuredText data={answer} />
       </span>
